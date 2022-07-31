03cabe0f9682c086f744146ad1d41179e55b7ffa
KGN-BOTS <108331307+KGN-BOTS@users.noreply.github.com>
2022-07-31 13:25:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/ZauteKm/Image-Upload-Bot.git /Image-Upload-Bot
 cd /Image-Upload-Bot
 pip3 install -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Amazing Bot Created By @KGN_MOVIES...."
 python3 main.py
